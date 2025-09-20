732e5ed5f36c698f6e830f7a1b93793e196e0c4a
Francisco Luvisari Scavassa <43516485+chicopsych@users.noreply.github.com>
2025-09-20 17:55:25 -0300

    Add initial index file and project build configuration
    - Created a new index file for the DesencriptacaoDeHexasDoRegWindowns project.
    - Added a build configuration file to skip analyzers during the build process.

--- obj/Debug/net8.0/DesencriptacaoDeHexasDoRegWindowns.AssemblyInfo.cs
+++ obj/Debug/net8.0/DesencriptacaoDeHexasDoRegWindowns.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DesencriptacaoDeHexasDoRegWindowns")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c35c12c3c7901bc6ba60abe4edc3250d97bec93c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4dd1d6172a943922aa948a530fc64cc9bd2f4a8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("DesencriptacaoDeHexasDoRegWindowns")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DesencriptacaoDeHexasDoRegWindowns")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
